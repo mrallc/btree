19b780be52c4a48c47b77fbed9897b1d74734349
mike andrews <mra@xoba.com>
2012-10-06 11:51:54 -0400

    getting ready to publish again

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
-an asynchronous copy-on-write b-tree in javascript; see
+an asynchronous copy-on-write (c.o.w.) b-tree in javascript; see
 http://en.wikipedia.org/wiki/B-tree for details on b-tree's.
+
+"asynchronous" means that all methods require a callback and return
+nothing directly. in other words, this implementation is event-driven.
+
+"cow" means that the entire b-tree is a pure immutable value,
+represented by its root key. all mutations create a new root key;
+i.e., a change three levels down in the b-tree causes modifications to
+the upper levels as well, including creating a new root that
+references that change down below.
+
+the two main methods are "add" and "scan". "get" and "create" are
+really just special cases of "add" and "scan". the b-tree relies on a
+key/value store, the simplest example of which is included as "kv.js".
+
+see "simple.js" for an ultra-simple demo, and "demo.js" for a more
+involved one. 
+
+bug reports and pull requests will be welcomed! thanks in advance.
 
 initially inspired by Justin Wetherell's java code at
 http://code.google.com/p/java-algorithms-implementation/
-
 
--- makepackage.js
+++ makepackage.js
@@ -1,38 +1,38 @@
 #!/usr/bin/env node
 
 var p = {
-    "name": "btree",
-    "version": "1.0.0",
-    "author": {
+    name: "btree",
+    version: "1.0.0",
+    author: {
 	name: "mike andrews",
 	email: "mra@xoba.com",
 	url: "http://MichaelRaskinAndrews.com"
     },
-    "homepage": "https://github.com/mrallc/btree",
-    "description": "asynchronous copy-on-write btree",
-    "scripts": {
-	"start": "node simple.js"
+    homepage: "https://github.com/mrallc/btree",
+    description: "asynchronous copy-on-write btree",
+    scripts: {
+	start: "node simple.js"
     },
-    "dependencies": {
-	"underscore": "1.4.1"
+    dependencies: {
+	underscore: "1.4.1"
     },
-    "repository": {
-	"type": "git",
-	"url": "git://github.com/mrallc/btree.git"
+    repository: {
+	type: "git",
+	url: "git://github.com/mrallc/btree.git"
     },
     bugs: {
-        "url" : "http://github.com/mrallc/btree/issues",
-        "mail" : "mra@xoba.com"
+        url : "http://github.com/mrallc/btree/issues",
+        mail : "mra@xoba.com"
     },
-    "licenses": [{
-        "type": "Apache License, Version 2.0",
-        "url": "http://github.com/mrallc/btree/raw/master/LICENSE-2.0"
+    licenses: [{
+        type: "Apache License, Version 2.0",
+        url: "http://github.com/mrallc/btree/raw/master/LICENSE-2.0"
     }],
-    "keywords": [
+    keywords: [
 	"btree","async","copy-on-write","cow","asynchronous","database"
     ],
-    "engine": {
-	"node": ">=0.8.0"
+    engine: {
+	node: ">=0.8.0"
     }
 }
 
